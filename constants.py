3272638617ec4e8213c85082dd6f68b8e097aa7b
paldy <58873376+paldy@users.noreply.github.com>
2019-12-27 14:47:37 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-token = '1002957496:AAFIg6mdexxGt6EjBc43G9NLbZ6BAPsWNCk'
-my_path = '/home/dipp/py/tgbot/'
-logfile = 'dataset_my.txt'
+token = '1002957496:AAFIg6mdexxGt6EjB'
+my_path = './py/tgbot/'
+logfile = 'dataset.log'
